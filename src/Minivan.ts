91caaf3828a97fb96f4289b39ffa2df304c014d2
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-04-18 13:23:56 +0700

    set Minivan

--- src/Minivan.ts
+++ src/Minivan.ts
@@ -8,7 +8,22 @@ export class MiniVan extends Vehicle{
         this.numberCustomer = numberCustomer;
         this.numberLuggages = numberLuggage;
     } 
-    getSpeed(): void {
-        throw new Error("Method not implemented.");
+    // getSpeed(): void {
+    //     throw new Error("Method not implemented.");
+        
+    // }
+    getSpeed(): number {
+        let speed = 130;
+        let decreased = 0;
+        let passenger = this.numberCustomer * 10;
+        let luggage = this.numberLuggages * 5;
+        if (decreased < speed){
+            decreased = speed - passenger;
+        }
+        else if(decreased < speed){
+            decreased = speed - luggage;
+        }
+        return decreased;
+
     }
 }
