0af7994f7ffe0c75547ecc7e5494e297da9ecb35
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-04-18 08:56:18 +0700

    update minivan

--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
  export abstract class Vehicle {
-=======
-export abstract class Vehicle {
->>>>>>> 54824d079be29e9943b14c7a13f03894c2de4f77
     private plateID:string;
     private weight:number;
     constructor(plateID:string,weight:number) {
