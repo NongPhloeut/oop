1190254c1b8444213f50138cc076df1f6310733c
THOEUN.SIENGDAV <THOEUN.SIENGDAV@PNC.LAN>
2023-04-18 09:26:13 +0700

    Get Speed of TucTuc

--- src/TucTuc.ts
+++ src/TucTuc.ts
@@ -5,4 +5,15 @@ export class TucTuc extends Vehicle{
         super(plateID, weight)
         this.numberCustomers = numberCustomers
     }
+    getSpeed(): number {
+        let remainSpeed = 0;
+        let maxSpeed = 130;
+        let passenger = this.numberCustomers * 5;
+        if(passenger < maxSpeed){
+            remainSpeed = maxSpeed - passenger;
+        }
+        return remainSpeed;
+        
+        
+    }
 }
