c6c884b5549f5add724c0cd3ca5dfedf149aa1db
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-04-18 09:00:34 +0700

    minivan

--- src/Minivan.ts
+++ src/Minivan.ts
@@ -1,10 +1,10 @@
 import {Vehicle} from "./Vehicle";
 export class MiniVan extends Vehicle{
     numberCustomer: number;
-    numberLuggage: number;
+    numberLuggages: number;
     constructor(plateID:string,weight:number, numberCustomer: number, numberLuggage: number){
         super(plateID,weight);
         this.numberCustomer = numberCustomer;
-        this.numberLuggage = numberLuggage;
+        this.numberLuggages = numberLuggage;
     }
 }
